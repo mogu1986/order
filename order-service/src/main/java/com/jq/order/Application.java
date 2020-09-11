c4bcf4d7d777c56adf541abe21c074e286ad459e
gaowei <1qaz2wsx>
2020-09-11 20:27:01 +0800

    debug

--- order-service/src/main/java/com/jq/order/Application.java
+++ order-service/src/main/java/com/jq/order/Application.java
@@ -1,10 +1,12 @@
 package com.jq.order;
 
+import com.jq.boot.feign.FeignConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Description: 启动类
@@ -13,6 +15,7 @@ import org.springframework.cache.annotation.EnableCaching;
  */
 @Slf4j
 @EnableCaching
+@EnableFeignClients(defaultConfiguration= FeignConfig.class)
 @SpringBootApplication(scanBasePackages = {"com.jq"})
 public class Application {
 
