25512b0422e651410d14893192f47226455febe2
gaowei <1qaz2wsx>
2020-09-12 01:54:08 +0800

    debug

--- order-service/src/main/java/com/jq/order/Application.java
+++ order-service/src/main/java/com/jq/order/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @Slf4j
 @EnableCaching
 @EnableFeignClients(defaultConfiguration= FeignConfig.class)
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.jq"})
 public class Application {
 
